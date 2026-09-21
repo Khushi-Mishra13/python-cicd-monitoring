c7863d54c04003a38c8c1dae0da409265863be5d
arjun.gahatraj <arjun.gahatraj@focalworks.in>
2026-09-21 10:29:33 +0530

    added prometheus metrics

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from prometheus-client import counter, gernerate_latest
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+	"app_requests_total",
+	"Total number of  requests")
 
 @app.route("/")
 def home() :
@@ -10,6 +15,11 @@ def home() :
 def health() :
 	return "OK", 200
 
+@app.route("/metrics")
+def metrics() :
+	return generate_latest(), 200, {
+		"Content-Type": "text/plain; version=0.04"
+	}
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8083)
 
